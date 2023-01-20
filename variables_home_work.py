5805533f162c01296d905ae9abbab84bad65a989
Aziret <sarylbekovaziret@gmail.com>
2023-01-20 21:17:05 +0600

    Initial commit

---
+++ variables_home_work.py
@@ -0,0 +1,12 @@
+a=2
+b=2
+c=2
+print(a+b+c)
+
+a=2
+b=3
+c=4
+d=5
+e=6
+f=5
+print(a+b+c+d+e+f)		
---
+++ varialbles_class_work.py
@@ -0,0 +1,15 @@
+404 # our calss (integer)
+1.5 # honda fit volume (float)
+0.4 # tonalka volume (float)
+
+"Hello world!" # it`s usual command 
+'Arthur'
+'Akimov'
+
+True
+False
+
+print(404)
+print(0.4)
+print('Arthur')
+print(True)
